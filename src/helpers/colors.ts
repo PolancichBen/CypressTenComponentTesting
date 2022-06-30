8e74f0bade8871eccf25f5fa0276edd24bbaf573
Benjamin Polancich <benjamin.p@salesboomerang.com>
2022-06-30 08:29:28 -0700

    Made a colors page

---
+++ src/helpers/colors.ts
@@ -0,0 +1,11 @@
+const blue = "#0000FF";
+const red = "#FF0000";
+const yellow = "#FFFF00";
+const pink = "#FFC0CB";
+const green = "#008000";
+const orange = "#FFA500";
+const purple = "#800080";
+
+export const colorsArray = [blue, red, yellow, pink, green, orange, purple];
+
+export { blue, red, yellow, pink, green, orange, purple };
--- src/pages/ColorsPage.tsx
+++ src/pages/ColorsPage.tsx
@@ -1,15 +1,58 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import { endsWith } from "cypress/types/lodash";
+import React, { FC, useState } from "react";
+import styled from "styled-components";
+
+import { colorsArray } from "../helpers/colors";
 
 const Container = styled.div`
   min-height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+const ColorsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const ColorBox = styled.div<{ color: string }>`
+  border: 1px solid black;
+  width: 80px;
+  height: 80px;
+  background-color: ${({ color }) => color};
+  margin: 0 0.5rem;
+`;
+
+const Header = styled.h1``;
+
+const SubHeader = styled.h2``;
+
+const ResetButton = styled.button`
+  margin-top: 3rem;
+  background-color: transparent;
+  padding: 15px 2rem;
+  border-radius: 15px;
 `;
 
 const ColorsPage: FC = () => {
-  return <Container>Hello World</Container>;
+  const [selectedColor, setSelectedColor] = useState("");
+  return (
+    <Container>
+      <Header>Colors</Header>
+      <ColorsWrapper>
+        {colorsArray.map((color) => (
+          <ColorBox onClick={() => setSelectedColor(color)} color={color}>
+            {color}
+          </ColorBox>
+        ))}
+      </ColorsWrapper>
+      <SubHeader>Selected Color</SubHeader>
+      <ColorBox color={selectedColor}>{selectedColor}</ColorBox>
+      <ResetButton onClick={() => setSelectedColor("")}>Reset</ResetButton>
+    </Container>
+  );
 };
 
 export default ColorsPage;
