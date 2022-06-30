aeb31b3c18d70db014aeda031450c029135be7de
Benjamin Polancich <benjamin.p@salesboomerang.com>
2022-06-30 08:43:00 -0700

    Created a simple modal page

--- src/components/molecules/modals/HelloModal.tsx
+++ src/components/molecules/modals/HelloModal.tsx
@@ -1,8 +1,19 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 1px solid black;
+  background-color: white;
+  border-radius: 15px;
+  width: 500px;
+  height: 375px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const HelloModal: FC = () => {
-  return <div>Hello Modal</div>;
+  return <Container>Hello Modal</Container>;
 };
 
 export default HelloModal;
--- src/pages/ModalPage.tsx
+++ src/pages/ModalPage.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import React, { FC } from "react";
+import styled from "styled-components";
+import HelloModal from "../components/molecules/modals/HelloModal";
+import { useModal } from "../hooks/UseModal";
 
 const Container = styled.div`
   min-height: 100%;
@@ -8,8 +10,42 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
+const Header = styled.h1`
+  margin-left: 1rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SummonButton = styled.button`
+  margin: 0 1rem;
+  background-color: transparent;
+  padding: 15px 2rem;
+  border-radius: 15px;
+`;
+
 const ModalPage: FC = () => {
-  return <Container>Hello World</Container>;
+  const [triggerModalOne, ModalOne] = useModal();
+  const [triggerModalTwo, ModalTwo] = useModal();
+
+  return (
+    <Container>
+      <Header>Modals</Header>
+      <ModalOne contents={HelloModal} />
+      <ModalTwo contents={HelloModal} requireResponse />
+      <Wrapper>
+        <SummonButton onClick={triggerModalOne}>
+          Summon A Regular Modal
+        </SummonButton>
+        <SummonButton onClick={triggerModalTwo}>
+          Summon A Required Modal
+        </SummonButton>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default ModalPage;
